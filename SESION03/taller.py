77546a9dba6c5344525aea2cbda4dbbb076b8963
hmurcia <42268927+hmurcia@users.noreply.github.com>
2024-08-28 19:09:21 -0500

    Presentación enunciado taller en clase

---
+++ SESION03/taller.py
@@ -0,0 +1,5 @@
+'''
+Crear un programa que permita crear objetos de la clase CuentaBancaria, cuyos atributos son: numeroCta, nombreCliente, saldoCta, fechaApertura, ultimoRetiro, ultimaConsignacion.
+Aplicar encapsulamiento y definir los métodos apropiados para controlar y gestionar los atributos de los objetos creados.
+Utilizar un menu para las diferentes opciones, tales como aperturaCta, consignar, retirar y transferencia entre otros.
+'''
